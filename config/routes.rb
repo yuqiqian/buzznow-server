e9c510969d05854c60f49e31ce0c57c45576932b
Yuqi <yuqi.qian@outlook.com>
2016-01-23 16:24:59 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -56,5 +56,5 @@ Buzznow::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  root "application#hello"
+  # root "application#hello"
 end
