4e28d011ed7141268bbcbade55455c0327fdc8c5
Yuqi <yuqi.qian@outlook.com>
2016-01-23 15:39:30 -0500

    First commit, trying to deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery with: :exception
+
+	def hello
+		render text:"<h1>Hello</h1><p>Welcome</p>"
+	end
 end
