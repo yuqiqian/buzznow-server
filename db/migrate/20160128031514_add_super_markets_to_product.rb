38922a438f052a933b391b78e092e45119a0fe59
Yuqi <yuqi.qian@outlook.com>
2016-01-30 23:25:36 -0500

    fixing pg error

--- db/migrate/20160128031514_add_super_markets_to_product.rb
+++ db/migrate/20160128031514_add_super_markets_to_product.rb
@@ -2,7 +2,7 @@ class AddSuperMarketsToProduct < ActiveRecord::Migration
   def change
     add_reference :products, :super_markets, index: true
 
-    add_foreign_key :products, :super_markets, column: :supermarkets_id
+    add_foreign_key :products, :super_markets, column: :super_markets_id
   end
 
 end
