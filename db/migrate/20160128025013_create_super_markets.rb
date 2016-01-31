b1526c5215cdf896d22922e854908de688b6be90
Yuqi <yuqi.qian@outlook.com>
2016-01-30 23:16:11 -0500

    fixing pg error

--- db/migrate/20160128025013_create_super_markets.rb
+++ db/migrate/20160128025013_create_super_markets.rb
@@ -2,10 +2,11 @@ class CreateSuperMarkets < ActiveRecord::Migration
   def change
     create_table :super_markets do |t|
       t.text :name
-      t.references :products, index: true, foreign_key: true
+      t.references :products, index: true
       t.text :picture
 
       t.timestamps null: false
     end
+    add_foreign_key :super_markets, :products, column: :products_id
   end
 end
