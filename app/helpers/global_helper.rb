a95efeea040f5fee57692f25f80cec2ee98cac52
Yuqi <yuqi.qian@outlook.com>
2016-02-25 21:02:35 -0500

    fix bugs

--- app/helpers/global_helper.rb
+++ app/helpers/global_helper.rb
@@ -1,17 +1,17 @@
 module GlobalHelper
 	def success_return
-		return {"msg":"OK", "ret":0}
+		return {"msg"=>"OK", "ret"=>0}
 	end
 
 	def fail_save
-		return {"msg":"saving failed", "ret":4}
+		return {"msg"=>"saving failed", "ret"=>4}
 	end
 
 	def fail_update
-		return {"msg":"update failed", "ret":5}
+		return {"msg"=>"update failed", "ret"=>5}
 	end
 
 	def fail_find
-		return {"msg":"cannot find this user", "ret":6}
+		return {"msg"=>"cannot find this user", "ret"=>6}
 	end
 end
